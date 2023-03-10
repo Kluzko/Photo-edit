6e381813638f88fe679283d2e6e9dd1efc8bc5b3
kluzko <kluzniakkuba@gmail.com>
2023-03-10 21:05:26 +0100

    Refactor imgui window intialization class

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,7 @@
-#include "view/Window.h"
-
-
-class MyWindow : public ImageEditor::UI::Window
-{
-public:
-	MyWindow(const std::string& title, int width, int height) : Window(title, width, height)
-	{
-	}
-
-protected:
-	void init() override
-	{
-
-	}
-
-	void shutdown() override
-	{
-
-	}
-
-	void render() override
-	{
-		ImGui::Text("Hello, ImGui!");
-	}
-};
-
+#include "App.h"
 
 int main() {
-	MyWindow window("Image Editor", 1200, 800);
-	window.run();
-
+	App app;
+	app.Run();
 	return 0;
 }
